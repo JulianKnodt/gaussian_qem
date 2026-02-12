d46bc65c3dccd8e974bbd07967f88e14c8515afc
Julian Knodt <julianknodt@gmail.com>
2026-02-12 17:59:38 -0500

    Update readme and parameters

--- readme.md
+++ readme.md
@@ -1,3 +1,38 @@
 # Gaussian (Splatting) QEM
 
 QEM implementation for simplifying Gaussian Splats.
+
+# Installation
+
+1. Install [Rust](https://rust-lang.org/tools/install/).
+
+Make sure to use the nightly toolchain.
+
+2. clone this repository
+
+```
+git clone git@github.com:JulianKnodt/gaussian_qem.git
+```
+or
+```
+git clone https://github.com/JulianKnodt/gaussian_qem.git
+```
+
+and then `cd gaussian_qem`.
+
+3. Build/Run the executable
+
+```
+cargo build --release
+./target/release/decimate -h
+```
+or
+```
+cargo run --release -- -h
+```
+
+- Example usage:
+
+```
+./target/release/decimate -i my_splat.ply -o out.ply -r 0.5
+```
--- src/parameters.rs
+++ src/parameters.rs
@@ -9,27 +9,27 @@ use clap::Parser;
                 .args(&["ratio", "number"]),
         ))]
 pub struct Args {
-    /// Input mesh file.
+    /// Input Splat file.
     #[arg(short, long, required = true)]
     pub input: String,
 
-    /// Output mesh file.
+    /// Output Splat file.
     #[arg(short, long, required = true)]
     pub output: String,
 
-    /// Output stat file.
+    /// Output stat file of run details.
     #[arg(long, default_value_t = String::new())]
     pub stats: String,
 
-    /// Approximate ratio of output/input tris.
+    /// Ratio of output splats to input splats (i.e. 0.5 = 50%)
     #[arg(short = 'r', long, group = "target")]
     pub ratio: Option<F>,
 
-    /// Approximate number of output tris.
+    /// Number of output splats
     #[arg(short = 'n', long, group = "target")]
     pub number: Option<usize>,
 
-    /// Do not use delta cost.
+    /// Do not use delta cost (probably don't need to change).
     #[arg(long)]
     pub no_delta_cost: bool,
 
@@ -41,11 +41,12 @@ pub struct Args {
     #[arg(long, default_value_t = 20)]
     pub k_nearest: usize,
 
-    /// Test the I/O without simplification
+    /// [DEBUG] Test I/O without simplification
     #[arg(long, hide = true)]
     pub test_io: bool,
 
-    /// Weight to use for preserving orientation of gaussians
+    /// Weight to use for preserving orientation of gaussians.
+    /// (Probably doesn't need to be changed).
     #[arg(long, default_value_t = 1e-4)]
     pub rot_weight: F,
 
